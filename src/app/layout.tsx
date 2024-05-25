45ca05c1441d626b714af296703dd8ea0c0837d6
Peter Boyd <peterboyd192@gmail.com>
2024-05-25 15:45:51 -0600

    fetched all geojson data and started working on interactivity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,31 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 // TODO: edit this
 export const metadata: Metadata = {
-  title: "Colorade Agrovoltaic Learning Center",
-  description: "A map of Solar Development in Colorado",
+    title: "Colorade Agrovoltaic Learning Center",
+    description: "A map of Solar Development in Colorado",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <Head>
+                <Script
+                    async
+                    src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&loading=async&callback=initMap`}
+                />
+            </Head>
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
--- src/components/interactive-map/interactive-map.tsx
+++ src/components/interactive-map/interactive-map.tsx
@@ -1,16 +1,84 @@
-import { GoogleMapsEmbed } from '@next/third-parties/google'
+'use client';
+
+import { useEffect, useRef, useState } from 'react'
+import { Loader } from '@googlemaps/js-api-loader';
+import styles from './interactive-map.module.css';
+import { GEOJSON_FILE_NAMES } from './interactive-map.utils'
+
+const loader = new Loader({
+    apiKey: "AIzaSyAxRMopOY_mVRQHK1TA8BxzMZEnWZHvnlc",
+    version: "weekly",
+    libraries: ["maps"]
+});
+
+const mapOptions = {
+    center: {
+        lat: 39.5501,
+        lng: -105.7821
+    },
+    region: 'CO',
+    zoom: 9
+};
+
+const setFeatures = (features: google.maps.Data.Feature[], index: number, countyName: string) => {
+    features.map((feature) => {
+        feature.setProperty('name', countyName);
+        feature.setProperty('index', index);
+    });
+}
 
 export default function InteractiveMap() {
+    const mapRef = useRef<google.maps.Map>();
+    const [mapSize, setMapSize] = useState({ width: 0, height: 0 });
+
+    useEffect(() => {
+        // load map
+        loader.importLibrary('maps').then(() => {
+            mapRef.current = new google.maps.Map(document.getElementById("map") as HTMLDivElement, mapOptions);
+
+            GEOJSON_FILE_NAMES.map((fileName, i) => {
+                const countyName = fileName.match(/(\w+)-county/)?.[1];
+                mapRef.current?.data.loadGeoJson(
+                    fileName,
+                    undefined,
+                    features => setFeatures(
+                        features,
+                        i,
+                        countyName as string
+                    )
+                );
+            });
+
+            mapRef.current?.data.setStyle((feature) => {
+                const index = Number(feature.getProperty('index'));
+                return {
+                    fillColor: (index % 2 === 0) ? 'red' : 'blue',
+                    strokeWeight: 1
+                }
+            });
+
+        });
+
+        // resize map
+        const handleResize = () => {
+            setMapSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            });
+        };
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [])
+
     return (
-        <GoogleMapsEmbed
-            apiKey={process.env.GOOGLE_MAPS_API_KEY as string}
-            height={'800px'}
-            width="100%"
-            mode='view'
-            zoom='9'
-            center={'39.5501,-105.7821'}
-            region='CO'
-            maptype='roadmap'
+        <div
+            id="map"
+            className={styles.Map}
+            style={{
+                width: `${mapSize.width}px`,
+                height: `${mapSize.height}px`
+            }}
         />
     )
 }
