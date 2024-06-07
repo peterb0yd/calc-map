92506ddb1bfe8370bea7fcd153b28ecb7ef811aa
Peter Boyd <peterboyd192@gmail.com>
2024-06-06 19:48:49 -0600

    improved legend and selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,17 @@ import styles from "./page.module.css";
 import { InteractiveMap } from "@/components/interactive-map/interactive-map";
 import { SideMenu } from "@/components/side-menu/side-menu";
 import { CountyService } from "@/api/county/county.service";
+import { Suspense } from "react";
 
 const Home = async () => {
     const counties = await CountyService.getCounties();
 
     return (
         <main className={styles.Main}>
-            <SideMenu counties={counties} />
-            <InteractiveMap counties={counties} />
+            <Suspense fallback={<div>Loading...</div>}>
+                <SideMenu counties={counties} />
+                <InteractiveMap counties={counties} />
+            </Suspense>
         </main>
     );
 }
